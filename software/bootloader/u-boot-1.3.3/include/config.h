c6af73a2f1544c784dbcfe63b5fe97850c28cb67
kuba.odias <kuba.odias@0cd42660-0ffe-11de-8ca9-75d9e183a346>
2009-06-12 08:16:42 +0000

    Initial import of u-boot files

---
+++ software/bootloader/u-boot-1.3.3/include/config.h
@@ -0,0 +1,2 @@
+/* Automatically generated - do not edit */
+#include <configs/lab_at91rm9200.h>
---
+++ software/bootloader/u-boot-1.3.3/include/config.mk
@@ -0,0 +1,5 @@
+ARCH   = arm
+CPU    = arm920t
+BOARD  = lab_at91rm9200
+VENDOR = atmel
+SOC    = at91rm9200
